37870337031b20f36eb9819fca4dac38c1304176
nelsontky <nelson@ccb.wtf>
2020-08-24 20:23:13 +0800

    Updates

---
+++ precache-manifest.9cd3522860f779fa65209d8aed4c350d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff10247e8618854b05cf265b01f20df",
+    "url": "/tutpool-landing-page/index.html"
+  },
+  {
+    "revision": "ef754f2c0711f3bd8353",
+    "url": "/tutpool-landing-page/static/js/2.f0f6924d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tutpool-landing-page/static/js/2.f0f6924d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8326b632a16d8cdf9312",
+    "url": "/tutpool-landing-page/static/js/main.a5bb1eec.chunk.js"
+  },
+  {
+    "revision": "e1fcc0aceb79d58e17df",
+    "url": "/tutpool-landing-page/static/js/runtime-main.fd871c41.js"
+  }
+]);
